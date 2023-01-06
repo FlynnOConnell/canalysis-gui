39e670067a6e14963d54c6d8eb287e00e93cf1d2
NeuroPyPy <Flynnoconnell@gmail.com>
2023-01-06 02:09:06 -0500

    rework structure

---
+++ gui_canalysis.h
@@ -0,0 +1,47 @@
+
+#pragma once
+
+#include "resource.h"
+#include "gui_canalysis.h"
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_vulkan.h"
+#include <stdio.h>          
+#include <stdlib.h>       
+#define GLFW_INCLUDE_NONE
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+
+
+static VkAllocationCallbacks* g_Allocator = NULL;
+static VkInstance               g_Instance = VK_NULL_HANDLE;
+static VkPhysicalDevice         g_PhysicalDevice = VK_NULL_HANDLE;
+static VkDevice                 g_Device = VK_NULL_HANDLE;
+static uint32_t                 g_QueueFamily = (uint32_t)-1;
+static VkQueue                  g_Queue = VK_NULL_HANDLE;
+static VkDebugReportCallbackEXT g_DebugReport = VK_NULL_HANDLE;
+static VkPipelineCache          g_PipelineCache = VK_NULL_HANDLE;
+static VkDescriptorPool         g_DescriptorPool = VK_NULL_HANDLE;
+
+static ImGui_ImplVulkanH_Window g_MainWindowData;
+static int                      g_MinImageCount = 2;
+static bool						g_SwapChainRebuild = false;
+
+void check_vk_result(VkResult err);
+
+void SetupVulkan(const char** extensions, uint32_t extensions_count);
+
+void SetupVulkanWindow(ImGui_ImplVulkanH_Window* wd, VkSurfaceKHR surface, int width, int height);
+
+void CleanupVulkan();
+
+void check_vk_result(VkResult err);
+
+void glfw_error_callback(int error, const char* description);
+
+void FramePresent(ImGui_ImplVulkanH_Window* wd);
+
+
+void CleanupVulkanWindow();
