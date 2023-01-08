c4a49c0a0cf8bb6fdd58c491a1bee889cc434c92
NeuroPyPy <Flynnoconnell@gmail.com>
2023-01-08 01:07:03 -0500

    vulkan -> OpenGL3

--- gui_canalysis.h
+++ gui_canalysis.h
@@ -11,35 +11,9 @@
 #define GLFW_INCLUDE_NONE
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#include <vulkan/vulkan.h>
 
-
-static VkAllocationCallbacks* g_Allocator = NULL;
-static VkInstance               g_Instance = VK_NULL_HANDLE;
-static VkPhysicalDevice         g_PhysicalDevice = VK_NULL_HANDLE;
-static VkDevice                 g_Device = VK_NULL_HANDLE;
-static uint32_t                 g_QueueFamily = (uint32_t)-1;
-static VkQueue                  g_Queue = VK_NULL_HANDLE;
-static VkDebugReportCallbackEXT g_DebugReport = VK_NULL_HANDLE;
-static VkPipelineCache          g_PipelineCache = VK_NULL_HANDLE;
-static VkDescriptorPool         g_DescriptorPool = VK_NULL_HANDLE;
-
-static ImGui_ImplVulkanH_Window g_MainWindowData;
 static int                      g_MinImageCount = 2;
 static bool						g_SwapChainRebuild = false;
 
-void check_vk_result(VkResult err);
-
-void SetupVulkan(const char** extensions, uint32_t extensions_count);
-
-void SetupVulkanWindow(ImGui_ImplVulkanH_Window* wd, VkSurfaceKHR surface, int width, int height);
-
-void CleanupVulkan();
-
-void check_vk_result(VkResult err);
-
-void FramePresent(ImGui_ImplVulkanH_Window* wd);
-
-void CleanupVulkanWindow();
 
 void ShowPlottingWindow(bool* p_open);
